12b27d1e6697275fe7236d218f4cecccf0d2f918
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2026-01-12 14:36:10 +0600

    Register Page layout
    Register Page Layout was Missing. Updated Now

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <title>register</title>
-</head>
-<body>
-    <div class="container">
+@extends('frontend.layout.master')
+@section('title','Register now')
+@section('content')
+
+<div class="container">
         <div class="row justify-content-center">
-            <h1>Registration Page :</h1>
             <div class="col-6 my-5">
+                <h1>Registration Page :</h1>
                 <form action="{{ route ('auth.post.register')}}" method="post">
                     @csrf
                     <div class="form-group">
@@ -38,6 +33,4 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-</html>
+    @endsection
